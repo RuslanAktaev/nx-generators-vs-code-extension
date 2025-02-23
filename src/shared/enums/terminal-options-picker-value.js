9a641a0fd71624cfce4c9df65bfc95aa51b47668
Ruslan Aktaev <second.roven@gmail.com>
2025-02-24 00:27:12 +0500

    refactor: Removed js files

--- src/shared/enums/terminal-options-picker-value.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TerminalOptionsPickerValue = void 0;
-var TerminalOptionsPickerValue;
-(function (TerminalOptionsPickerValue) {
-    TerminalOptionsPickerValue["EXECUTE_COMMAND"] = "execute_command";
-})(TerminalOptionsPickerValue || (exports.TerminalOptionsPickerValue = TerminalOptionsPickerValue = {}));
-//# sourceMappingURL=terminal-options-picker-value.js.map
